78b9e46e21fd7b4b7184e0e04ee5d559b7a5a0a5
Nisha Verma <85985602+nishaverma03@users.noreply.github.com>
2026-06-19 15:24:20 +0530

    Time: 234 ms (58.85%) | Memory: 143.5 MB (91.39%) - LeetSync

--- 3982-maximum-walls-destroyed-by-robots/maximum-walls-destroyed-by-robots.java
+++ 3982-maximum-walls-destroyed-by-robots/maximum-walls-destroyed-by-robots.java
@@ -1,49 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int maxWalls(int[] robots, int[] distance, int[] walls) {
         int n = robots.length;
-
         int[][] r = new int[n][2];
         for (int i = 0; i < n; i++) {
             r[i][0] = robots[i];
             r[i][1] = distance[i];
         }
-
         Arrays.sort(r, Comparator.comparingInt(a -> a[0]));
         Arrays.sort(walls);
-
         long[] left = new long[n];
         long[] right = new long[n];
-
         for (int i = 0; i < n; i++) {
             long pos = r[i][0];
             long dist = r[i][1];
-
-            // Shooting left
             long L = pos - dist;
             if (i > 0) {
                 L = Math.max(L, (long) r[i - 1][0] + 1);
             }
             left[i] = countWalls(walls, L, pos);
-
-            // Shooting right
             long R = pos + dist;
             if (i < n - 1) {
                 R = Math.min(R, (long) r[i + 1][0] - 1);
             }
             right[i] = countWalls(walls, pos, R);
         }
-
         long[] overlap = new long[Math.max(0, n - 1)];
-
         for (int i = 0; i < n - 1; i++) {
             long pos1 = r[i][0];
             long pos2 = r[i + 1][0];
-
             long rL = pos1;
             long rR = Math.min(pos1 + (long) r[i][1], pos2 - 1L);
-
             long lL = Math.max(pos2 - (long) r[i + 1][1], pos1 + 1L);
             long lR = pos2;
             long start = Math.max(rL, lL);
@@ -78,22 +65,18 @@ class Solution {
         int l = 0, r = arr.length;
         while (l < r) {
             int mid = l + (r - l) / 2;
-
             if (arr[mid] >= target) {
                 r = mid;
             } else {
                 l = mid + 1;
             }
         }
-
         return l;
     }
     private int upperBound(int[] arr, long target) {
         int l = 0, r = arr.length;
-
         while (l < r) {
             int mid = l + (r - l) / 2;
-
             if (arr[mid] > target) {
                 r = mid;
             } else {
