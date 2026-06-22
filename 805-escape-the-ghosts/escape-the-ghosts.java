10b63444a8eb121406c2be285dea6358d6edc629
Nisha Verma <85985602+nishaverma03@users.noreply.github.com>
2026-06-22 18:08:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (20.27%) - LeetSync

---
+++ 805-escape-the-ghosts/escape-the-ghosts.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int myDist = Math.abs(target[0]) + Math.abs(target[1]);
+        for (int[] ghost : ghosts) {
+            int ghostDist = Math.abs(ghost[0] - target[0]) +
+                            Math.abs(ghost[1] - target[1]);
+            if (ghostDist <= myDist) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
