b2de7ff96fb1002e1fabe6fccd438bd1864e1426
Nisha Verma <85985602+nishaverma03@users.noreply.github.com>
2026-06-28 13:21:07 +0530

    Time: 38 ms (5.81%) | Memory: 100 MB (24.62%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int value) {
+        stack.push(value);
+
+        if (minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
